bf52866b580c235295fa1f34d0181fb094a20e57
ZyberLock <151535871+ZyberLock@users.noreply.github.com>
2026-07-21 17:49:45 -0400

    Update install.ps1

--- bootstrap/install.ps1
+++ bootstrap/install.ps1
@@ -1,2 +1,11 @@
-# ADM Toolbox Bootstrap
-Write-Host "Bootstrap is working!"
+$ErrorActionPreference = 'Stop'
+
+$AppPath = Join-Path $env:LOCALAPPDATA 'ADM Toolbox\ADMToolbox.exe'
+
+if (Test-Path $AppPath) {
+    Start-Process -FilePath $AppPath
+    return
+}
+
+Write-Host 'ADM Toolbox is not installed yet.'
+Write-Host 'The installer download step will be added next.'
